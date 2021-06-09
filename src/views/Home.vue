98b1aea7de503f380e781fe785aa36fa1c45df8a
magdamk <75847988+magdamk@users.noreply.github.com>
2021-06-09 12:19:11 +0200

    1 miejsce

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,44 @@
 <template>
   <div class="home">
-    
+  <img v-bind:alt="name" v-bind:src="photo" style="height:200px;"/>
+    <h2>{{name}}</h2>
+    <div>
+      <p><strong>Opis: </strong>{{description}}</p>
+      <p><strong>Współrzędne: </strong>{{geoposition}}</p>
+      <p v-if="costToVisit"><strong>Koszt: </strong>{{costToVisit}} zł</p>
+      <p v-else>Za darmo!</p>
+      <button v-on:click="revealWeather()">Pogoda</button>
+      <p v-if="showWeather">Słonecznie</p>
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+//import HelloWorld from "@/components/HelloWorld.vue";
 
 export default {
   name: "Home",
-  components: {
-    HelloWorld,
+  data() {
+    return {
+      name: "Zamek Królewski w Warszawie",
+      photo:"https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.lBmk_8fsyQsS7d7rBZkKcgHaET%26pid%3DApi&f=1",
+      description: "Zamek Królewski w Warszawie – barokowo-klasycystyczny zamek królewski znajdujący się w Warszawie przy placu Zamkowym 4.",
+      costToVisit: 30,
+      longitude: 52.247778,
+      latitude: 21.01416,
+      showWeather: false
+    };
   },
-};
+  computed: {
+    geoposition(){
+      return this.longitude + ', ' + this.latitude;
+    }},
+    methods:{
+      revealWeather(){
+        this.showWeather = !this.showWeather;
+      }
+},
+  components: {}
+}
 </script>
